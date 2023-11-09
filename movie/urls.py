0d2417d11383efe514024b4bb83449e7aed0e9ce
fatih <afyildiz@gmail.com>
2023-11-09 20:54:07 +0300

    crud ok

--- movie/urls.py
+++ movie/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("", views.getHomepage, name='homepage'),
     path("home/", views.getHomepage, name='homepage'),
     path("add/", views.add_movie, name='add',),
+    path('update/<int:id>/', views.update, name='update'),
+    # path("edit/<int:id>", views.edit_movie, name='edit',),
     path("__debug__/", include("debug_toolbar.urls")),
     # path('add_movie/', views.add_movie, name='add_movie'),
     path("delete/<int:id>", views.delete_movie, name='delete')
--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,6 @@
 from datetime import timezone
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 
 from .forms import AddMovies
 from .models import Movies
@@ -8,11 +8,9 @@ from .models import Movies
 # Create your views here.
 def add_movie(request):
     templateName='add_movie.html'
-    print('1')
     if request.method == "POST":
         form = AddMovies(request.POST)
         if form.is_valid():
-            print('2')
             form.save()
             return redirect('homepage')
         else:
@@ -27,6 +25,23 @@ def delete_movie(request,id):
         movie.delete()
         return redirect('homepage')
 
+def edit_movie(request,id):
+     movie=Movies.objects.get(id=id)
+     return render(request,"edit_movie.html", {'data':movie})
+
+
+def update(request, id):
+    movie_instance = Movies.objects.get(pk=id)
+
+    if request.method == 'POST':
+        form = AddMovies(request.POST, instance=movie_instance)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+    else:
+        form = AddMovies(instance=movie_instance)
+
+    return render(request, 'edit_movie.html', {'form': form,'data':movie_instance})
 
 def getHomepage(request):
     data={
